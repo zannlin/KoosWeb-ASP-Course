db3bb77e8f327466795581d61f438dc593a63254
zannlin <zanderlindeque1@gmail.com>
2025-05-19 13:33:11 +0200

    Add project files.

---
+++ KoosWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KoosWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

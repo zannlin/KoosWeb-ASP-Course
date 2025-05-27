2632e4ee59a04008e098f796247d45cf4994310d
zannlin <zanderlindeque1@gmail.com>
2025-05-27 21:27:17 +0200

    Added the database

--- KoosWeb/Program.cs
+++ KoosWeb/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<KoosWeb.Data.ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DeafaultConnection")));
 
 var app = builder.Build();
 

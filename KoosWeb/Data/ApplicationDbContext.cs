1a0a7f5da0f525260bce58faa27c1d3cc0219bb1
zannlin <zanderlindeque1@gmail.com>
2025-07-13 21:53:38 +0200

    feat: add category controller functionality add demo data and to view all and display in index of category

--- KoosWeb/Data/ApplicationDbContext.cs
+++ KoosWeb/Data/ApplicationDbContext.cs
@@ -7,9 +7,19 @@ namespace KoosWeb.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Category> Categories { get; set; }
-    }   
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasData(
+                    new Category { Id = 1, Name = "Electronics", DisplayOrder = 1 },
+                    new Category { Id = 2, Name = "Books", DisplayOrder = 2 },
+                    new Category { Id = 3, Name = "Clothing", DisplayOrder = 3 }
+                );
+        }
+    }
 }

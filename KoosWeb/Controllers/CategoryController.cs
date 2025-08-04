589f79b6a4610d1a1682218574d6d82a0969592a
zannlin <zanderlindeque1@gmail.com>
2025-08-04 13:31:08 +0200

    Feat: Delete functionality
    - Add delete view
    - Add delete GET and POST

--- KoosWeb/Controllers/CategoryController.cs
+++ KoosWeb/Controllers/CategoryController.cs
@@ -22,11 +22,11 @@ namespace KoosWeb.Controllers
         {
             return View();
         }
-        
+
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
             }
@@ -63,6 +63,34 @@ namespace KoosWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
